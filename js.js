3d15c5a0c52fea22bca3efba2df3a52fcf332219
FELIPE NASCIMENTO DE MORAIS <26150@unifacef.edu.br>
2025-09-02 22:12:53 -0300

    b

--- js.js
+++ js.js
@@ -33,3 +33,28 @@ VetApro
 let vet = beecrowd.split("\n")
 
 
+
+let palavra = prompt("Informe uma palavra")
+// tira os espaços antes e depois
+let limpa = palavra.trim()
+// cria um vetor da palavra limpa
+let vetor = limpa.split("")
+// inverte palavra do tipo vetor
+let invertida = vetor.reverse()
+// converte vetor em string
+let invertidaString = invertida.join("")
+
+if (invertidaString === limpa){ // compara duas strings
+    alert('São palíndromas')
+}
+else alert('Não são palíndromas')
+/*
+
+let palavra = prompt("Informe uma palavra")
+let invertida = palavra.trim().split("").reverse().join()
+
+if (palavra.trim() === invertida){ // compara duas strings
+    alert('São palíndromas')
+}
+else alert('Não são palíndromas') */
+:)
