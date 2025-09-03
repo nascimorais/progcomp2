54d6941f5c1c844cecb16c08f619b11a60babcab
FELIPE NASCIMENTO DE MORAIS <26150@unifacef.edu.br>
2025-09-02 22:05:36 -0300

    a

---
+++ js.js
@@ -0,0 +1,35 @@
+let frase = "estou estudando JS"
+
+frase
+//`estou estudando JS`
+frase[4]
+//`u`
+frase.length
+//`18`
+frase.toUpperCase()
+//`ESTOU ESTUDANDO JS`
+frase.toLowerCase{}
+//`estou estudando js`
+frase.toUpperCase().includes(`js`)
+//`false`
+frase.toUpperCase().includes(`JS`)
+//`true`
+frase.slice(-5)
+//`do JS`
+
+let frase = "    hoje, pra variar foi um dia corrido       "  
+frase.trim()
+//"hoje, pra variar foi um dia corrido"  
+let resumo = "Hoje foi um dia com muitas atividades tensas"
+resumo.length
+//44
+resumo.split(" ").lenght
+//8
+let aprovados =
+"João,José,pedro"
+let VetApro = aprovados.split(",")
+VetApro 
+//['João','José','pedro']
+let vet = beecrowd.split("\n")
+
+
